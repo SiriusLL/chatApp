156a1bdf55900422a1d02a8f13c1532fbf596980
SiriusLL <sirius.love.light@gmail.com>
2021-05-31 18:09:22 -0600

    create socket connection in server

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ const PORT = 8080;
 http.listen(PORT, () => {
   console.log(`listening on *:${PORT}`);
 });
+
+io.on("connection", (socket) => {
+  /* socket object may be used to send specific messages to the new connected client */
+  console.log("new client connected");
+});
